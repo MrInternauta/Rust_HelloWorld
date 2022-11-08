7395a75fa4fa6b13bf6c733cd083d7e2a61e998d
Felipe Rmirez <feldjesus@gmail.com>
2022-11-08 05:41:49 -0600

    Data typs

--- src/main.rs
+++ src/main.rs
@@ -67,4 +67,56 @@ fn main() {
     println!("{}, level {}. Remote: {}. Grades: {}, {}, {}, {}. Average: {}", 
             user_2.name, user_2.level, user_2.remote, mark_2.0, mark_2.1, mark_2.2, mark_2.3, mark_2.4);
 
+  enum WebEvent {
+    // An enum variant can be like a unit struct without fields or data types
+    WELoad,
+    // An enum variant can be like a tuple struct with data types but no named fields
+    WEKeys(String, char),
+    // An enum variant can be like a classic struct with named fields and their data types
+    WEClick { x: i64, y: i64 }
+  }
+
+    // Define a tuple struct
+    #[derive(Debug)]
+    struct KeyPress(String, char);
+
+    // Define a classic struct
+    #[derive(Debug)]
+    struct MouseClick { x: i64, y: i64 }
+
+   //Simple variant: WELoad(bool) 
+    // Redefine the enum variants to use the data from the new structs
+    // Update the page Load variant to have the boolean type
+    // The first variant in the WebEvent enum has a single boolean value, WELoad(bool). We instantiate this variant in a manner similar to how we worked with booleans in the previous unit:
+    #[derive(Debug)]
+    enum WebEvent2 { WELoad(bool), WEClick(MouseClick), WEKeys(KeyPress) }
+
+    let we_load: WebEvent2 = WebEvent2::WELoad(true);
+
+    //Struct variant: WEClick(MouseClick)
+    // Instantiate a MouseClick struct and bind the coordinate values
+    let click = MouseClick { x: 100, y: 250 };
+
+    // Set the WEClick variant to use the data in the click struct
+    let we_click = WebEvent2::WEClick(click);
+
+    //Tuple variant: WEKeys(KeyPress)
+
+    // Instantiate a KeyPress tuple and bind the key values
+    let keys = KeyPress(String::from("Ctrl+"), 'N');
+        
+    // Set the WEKeys variant to use the data in the keys tuple
+    let we_key = WebEvent2::WEKeys(keys);
+
+        
+  // Print the values in the WebEvent enum variants
+  // Use the {:#?} syntax to display the enum structure and data in a readable form
+  println!("\nWebEvent enum structure: \n\n {:#?} \n\n {:#?} \n\n {:#?}", we_load, we_click, we_key);
+  goodbye();
+}
+
+
+
+fn goodbye() {
+  println!("Goodbye.");
 }
